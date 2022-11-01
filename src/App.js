b6874ad7e3fdd54b77b39e9a42c597434924d628
PawanVedpathak <80150887+Vepathakpawan@users.noreply.github.com>
2022-11-02 02:01:13 +0530

    excel data fetch

--- src/App.js
+++ src/App.js
@@ -1,23 +1,73 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React, { useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import * as XLSX from "xlsx";
+import "../src/Assets/style.css";
 function App() {
+  const [items, setItems] = useState([]);
+
+  const readExcel = (file) => {
+    const promise = new Promise((resolve, reject) => {
+      const fileReader = new FileReader();
+      fileReader.readAsArrayBuffer(file);
+
+      fileReader.onload = (e) => {
+        const bufferArray = e.target.result;
+
+        const wb = XLSX.read(bufferArray, { type: "buffer" });
+
+        const wsname = wb.SheetNames[0];
+
+        const ws = wb.Sheets[wsname];
+
+        const data = XLSX.utils.sheet_to_json(ws);
+
+        resolve(data);
+      };
+
+      fileReader.onerror = (error) => {
+        reject(error);
+      };
+    });
+
+    promise.then((d) => {
+      setItems(d);
+    });
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className="input-wrapper">   
+          <input
+        type="file" className="choose-input" id="btn"
+        onChange={(e) => {
+          const file = e.target.files[0];
+          readExcel(file);
+        }}
+      />
+      </div>
+ 
+
+      <table class="table container">
+        <thead>
+          <tr>
+            <th scope="col">ChildPartNumber</th>
+            <th scope="col">ChildPartDescription</th>
+            <th scope="col">itemReferenceNumber</th>
+            <th scope="col">QuantityProduction</th>
+          </tr>
+        </thead>
+        <tbody>
+          {items.map((d) => (
+            <tr key={d.ChildPartNumber}>
+              <td>{d.ChildPartNumber}</td>
+              <td>{d.ChildPartDescription}</td>
+              <td className="text-right" >{d.itemReferenceNumber}</td>
+              <td className="text-right">{d.QuantityProduction}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
   );
 }
